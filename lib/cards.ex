a40763d882fdcf37eb86654b737272a0b1027aa1
mplibunao <markpaololibunao@gmail.com>
2018-03-02 13:30:57 +0800

    save dec

--- lib/cards.ex
+++ lib/cards.ex
@@ -20,4 +20,10 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    # Doesn't really convert to binary
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
 end
