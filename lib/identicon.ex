622d94058b97fea122803e1720a6b16a09ebd915
mplibunao <markpaololibunao@gmail.com>
2018-03-08 00:50:12 +0800

    USes struct to store the output of hash input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,7 +9,14 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
+<<<<<<< HEAD
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+=======
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+>>>>>>> 852ab69... Adds hash input function
   end
 end
