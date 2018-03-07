e6ad038080e2e167c346ff8cea73ff09af10bd3c
mplibunao <markpaololibunao@gmail.com>
2018-03-08 01:02:12 +0800

    Changes to a more concise syntax

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,9 +10,7 @@ defmodule Identicon do
   end
 
   def pick_color(image) do
-    %Identicon.Image{hex: hex_list} = image
-    [r, g, b | _tail ] = hex_list
-
+    %Identicon.Image{hex: h[r, g, b | _tail ]} = image
     [r, g, b]
   end
 
