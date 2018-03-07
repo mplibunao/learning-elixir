acb1a180a6c759e8e7daaeae1bc78df47de25a93
mplibunao <markpaololibunao@gmail.com>
2018-03-07 20:57:14 +0800

    Created stub for main function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,16 +3,12 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
+  def main(input) do
+    input
+    |> hash_input
+  end
 
-  ## Examples
+  def hash_input(input) do
 
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
   end
 end
