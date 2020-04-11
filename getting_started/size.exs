f9f47c290b2170f979c65389f1eee86cd4888607
MP <markpaololibunao@gmail.com>
2020-04-11 14:16:08 +0800

    Sample implementation of protocol

---
+++ getting_started/size.exs
@@ -0,0 +1,25 @@
+defprotocol Size do
+  @fallback_to_any true # prefer @derive over this
+  @doc "Calculates the size (and not the length!) of a data structure"
+  def size(data)
+end
+
+defimpl Size, for: BitString do
+  def size(string), do: byte_size(string)
+end
+
+defimpl Size, for: Map do
+  def size(map), do: map_size(map)
+end
+
+defimpl Size, for: Tuple do
+  def size(tuple), do: tuple_size(tuple)
+end
+
+defimpl Size, for: MapSet do
+  def size(set), do: MapSet.size(set)
+end
+
+defimpl Size, for: Any do
+  def size(_), do: 0
+end
--- getting_started/user.exs
+++ getting_started/user.exs
@@ -3,8 +3,13 @@ defmodule User do
   # defstruct [name: "John", age: 27, :email] # returns an error because nil fields are at the end
 end
 
+defimpl Size, for: User do
+  def size(_user), do: 3
+end
+
 # enforce certain keys have to be specified when creating struct
 defmodule Car do
   @enforce_keys [:make]
+  @derive [Size] # will implement Size protocol for Car based on implem for Any
   defstruct [:model, :make]
 end
