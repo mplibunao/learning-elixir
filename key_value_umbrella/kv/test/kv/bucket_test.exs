4365404d4171a35f6119d99c035789c83e6f2201
MP <markpaololibunao@gmail.com>
2020-05-25 17:40:28 +0800

    Creates bucket module which interfaces with Agent for managing state

---
+++ key_value_umbrella/kv/test/kv/bucket_test.exs
@@ -0,0 +1,21 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key and returns the deleted value if it exists", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 1)
+    assert KV.Bucket.delete(bucket, "milk") == 1
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+end
