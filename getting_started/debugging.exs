190556ae0733d27fc098f6d14419b154117492ac
MP <markpaololibunao@gmail.com>
2020-05-25 13:48:13 +0800

    Adds examples of debugging with IO.inspect using some of the available options

---
+++ getting_started/debugging.exs
@@ -0,0 +1,15 @@
+defmodule Debugging do
+  def some_fun(a,b,c) do
+    IO.inspect binding()
+  end
+end
+
+# binding returns the function args + their values
+Debugging.some_fun(1,2,3)
+
+# using the label opts in IO.inspect
+[1,2,3]
+|> IO.inspect(label: "before")
+|> Enum.map(& &1 *2)
+|> IO.inspect(label: "after")
+|> Enum.sum
