0033e11b8bb8f5f85ebe6c9ae8406a3c4f566b70
MP <markpaololibunao@gmail.com>
2020-06-02 10:21:04 +0800

    Creates a dynamic supervisor under the registry supervisor to supervise all the buckets. This prevents crashed buckets from crashing the registry since it's also linked to the registry. Changes supervisor strategy to `one_for_all` so that when Registry is restarted, KV.BucketSupervisor and all it's children are also restarted to prevent orphan processes

--- key_value_umbrella/kv/test/kv/bucket_test.exs
+++ key_value_umbrella/kv/test/kv/bucket_test.exs
@@ -18,4 +18,8 @@ defmodule KV.BucketTest do
     assert KV.Bucket.delete(bucket, "milk") == 1
     assert KV.Bucket.get(bucket, "milk") == nil
   end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
 end
