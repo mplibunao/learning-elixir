177a84f6aee3a6f79176c6295f8e8944edbcf32c
MP <markpaololibunao@gmail.com>
2024-05-13 12:58:41 +0800

    Adds setup that merges bucket into the test context making tests dry

--- getting_started/mix_and_otp/kv/test/kv/bucket_test.exs
+++ getting_started/mix_and_otp/kv/test/kv/bucket_test.exs
@@ -1,8 +1,13 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores value by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket} # ExUnit will merge this into the test context
+  end
+
+  test "stores value by key", %{bucket: bucket} do
+    # access bucket from test context
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
