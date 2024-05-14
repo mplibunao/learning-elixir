2656ebbe43b4276b955430d3a0fbc3c4da613e64
MP <markpaololibunao@gmail.com>
2024-05-14 18:02:47 +0800

    `KV.Registry` is both linking (via `start_link`) and monitoring (via `monitor`) bucket processes in the `handle_cast/2` callback:
    ```
    {:ok, bucket} = KV.Bucket.start_link([])
    ref = Process.monitor(bucket)
    ```
    
    Links are bidirectional, which implies that a crash in a bucket will crash the registry. Although we now have the supervisor, which guarantees the registry will be back up and running, crashing the registry still means we lose all data associating bucket names to their respective processes.
    
    ```
    iex(1)> KV.Registry.create(KV.Registry, "shopping")
    :ok
    iex(2)> {:ok, shopping} = KV.Registry.lookup(KV.Registry, "shopping")
    {:ok, #PID<0.154.0>}
    iex(3)> KV.Bucket.put(shopping, "milk", 1)
    :ok
    iex(4)> KV.Bucket.get(shopping, "milk")
    1
    
    iex(5)> KV.Registry.create(KV.Registry, "time_sheet")
    :ok
    iex(6)> {:ok, time_sheet} = KV.Registry.lookup(KV.Registry, "time_sheet")
    {:ok, #PID<0.155.0>}
    iex(7)> time_sheet
    iex(8)> KV.Bucket.put(time_sheet, "monday", 8)
    :ok
    iex(9)> KV.Bucket.get(time_sheet, "monday")
    8
    
    Agent.stop(time_sheet, :shutdown)
    
    KV.Registry.lookup(KV.Registry, "shopping") # error means shopping bucket is not there anymore eventhough only time_sheet bucket crashed
    ```
    
    - We solve this by using a `DynamicSupervisor` to spawn and supervise all buckets
    - Since children are not known upfront and started dynamically, this supervisor is optimized for this use case
    - Since it doesn't need children like Supervisor, we can just define it directly instead of creating a dedicated module for it, just make sure to name it so we can reference it when call `DynamicSupervisor.start_child/2` (which we use instead of `KV.Bucket.start_link` to create a bucket)
    
    ```
    def init(:ok) do
      children = [
        {KV.Registry, name: KV.Registry},
        {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
      ]
    
      Supervisor.init(children, strategy: :one_for_one)
    end
    
    iex(1)> {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
    {:ok, #PID<0.153.0>}
    iex(2)> KV.Bucket.put(bucket, "eggs", 3)
    :ok
    iex(3)> KV.Bucket.get(bucket, "eggs")
    ```

--- getting_started/mix_and_otp/kv/lib/kv/registry.ex
+++ getting_started/mix_and_otp/kv/lib/kv/registry.ex
@@ -44,7 +44,7 @@ defmodule KV.Registry do
     if Map.has_key?(names, name) do
       {:noreply, {names, refs}}
     else
-      {:ok, bucket} = KV.Bucket.start_link([])
+      {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
       ref = Process.monitor(bucket)
       refs = Map.put(refs, ref, name)
       names = Map.put(names, name, bucket)
--- getting_started/mix_and_otp/kv/lib/kv/supervisor.ex
+++ getting_started/mix_and_otp/kv/lib/kv/supervisor.ex
@@ -40,7 +40,8 @@ defmodule KV.Supervisor do
   @impl true
   def init(:ok) do
     children = [
-      {KV.Registry, name: KV.Registry}
+      {KV.Registry, name: KV.Registry},
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
