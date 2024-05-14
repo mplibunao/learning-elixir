a242535e2fdb72555af552bd7815a26d5ef51a21
MP <markpaololibunao@gmail.com>
2024-05-14 19:53:19 +0800

    - There is a resource leak with our implementation of `DynamicSupervisor` - When a bucket terminates, the supervisor will start a new bucket in its place. However, when the supervisor restarts the new bucket, the registry does not know about it. So we will have an empty bucket in the supervisor that nobody can access - To solve this, we want to say that buckets are actually temporary. If they crash, regardless of the reason, they should not be restarted. - We can do this by passing the `restart: :temporary` option to `use Agent` in `KV.Bucket`   - See supervisor restart options for more info - ordering and supervision strategy matters wrt supervision trees   - Order: Since `KV.Registry` invokes `KV.BucketSupervisor`, then the `KV.BucketSupervisor` must be started before `KV.Registry`. Otherwise, it may happen that the registry attempts to reach the bucket supervisor before it has started.   - Strategy: If `KV.Registry` dies, all information linking `KV.Bucket` names to bucket processes is lost. Therefore the `KV.BucketSupervisor` and all children must terminate too - otherwise we will have orphan processes.

--- getting_started/mix_and_otp/kv/test/kv/bucket_test.exs
+++ getting_started/mix_and_otp/kv/test/kv/bucket_test.exs
@@ -20,4 +20,8 @@ defmodule KV.BucketTest do
     assert KV.Bucket.delete(bucket, "apple") == 1
     assert KV.Bucket.get(bucket, "apple") == nil
   end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
 end
