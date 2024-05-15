daf4a5559ce339f73d1492596e337c58d37ef758
MP <markpaololibunao@gmail.com>
2024-05-15 20:33:04 +0800

    - erlang's `:gen_tcp` module to serve requests - start tcp server by implementing echo server (send response with text it received) - tcp server at high level does the ff: 	- listens to port until port is available and it gets hold of the socket 	- waits for client connection on port and accepts it 	- reads client request and writes response back
    ```
    iex(1)> KVServer.accept(4040)
    
    15:33:26.398 [info] Accepting connections on port 4040
    
    ❯ telnet 127.0.0.1 4040
    Trying 127.0.0.1...
    Connected to localhost.
    Escape character is '^]'.
    hello
    hello
    is it me
    is it me
    you are looking for?
    you are looking for?
    ```
    
    - To handle the case that `KVServer` crashes, we can start a `Task` through the application callback module and run `KVServer.accept/1` there
    - We read the port from env and run the application using `PORT=4321 mix run --no-halt`
    - When we try to connect using 2 telnet clients at the same time, the 2nd one doesn't echo
    	- This is because we are serving requests in the same process that are accepting connections. When one client is connected, we can't accept another client
    	- We can solve this by spawning a `Task` (`Task.start_link`) to handle `serve/1`
    	- But if the process serving response crashes, it brings down the acceptor with it due to the processes being linked consequently **bringing down all connections**
    	- solution: use a one for one supervisor. `TaskSupervisor` was made exactly for this
    - We also add the ff line:
    
    ```
    :ok = :gen_tcp.controlling_process(client, pid)
    ```
    
    - Now quitting a telnet client does not bring the acceptor down (hence bringing down other telnet clients)
    - Tasks have a `:restart` value of `:temporary` by default (not restarted)
    	- good default for the connections started by `Task.Supervisor`
    	- But bad choice for acceptor
    	- We can change specification by using `Supervisor.child_spec`
    - Summary:
    	- if acceptor crashes, existing connections don't crash too
    	- if task supervisor crashes, doesn't crash the acceptor too

--- getting_started/mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ getting_started/mix_and_otp/kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -7,9 +7,14 @@ defmodule KVServer.Application do
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg)
       # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      # Modify the Task child_spec by using `Supevisor.child_spec/2`
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
